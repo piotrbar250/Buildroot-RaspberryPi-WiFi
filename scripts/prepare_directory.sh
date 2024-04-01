7a080948b1b39d479e1e9ddb23df76fc91a0d682
piotrbar250 <piotr.baranowski250@gmail.com>
2024-04-01 02:15:57 +0200

    added prepare_directory script

--- scripts/prepare_directory.sh
+++ scripts/prepare_directory.sh
@@ -1,5 +1,4 @@
-cd ..
-tar -xJf buildroot-2023.11.1.tar.xz
-rsync -av --ignore-existing buildroot-2023.11.1 buildroot
-rm -rf buildroot-2023.11.1
-mkdir ccache-br
+tar -xJf ../buildroot-2023.11.1.tar.xz
+rsync -av --ignore-existing ../buildroot-2023.11.1 ../buildroot
+rm -rf ../buildroot-2023.11.1
+mkdir ../ccache-br
